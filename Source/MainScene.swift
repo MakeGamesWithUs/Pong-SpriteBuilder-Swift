18c4b277c564c22470a788482d87461b2d5b3d2d
Ben-G <Benjamin.Encz@gmail.com>
2015-01-20 11:28:25 -0800

    CPU implemented

--- Source/MainScene.swift
+++ Source/MainScene.swift
@@ -7,17 +7,16 @@ class MainScene: CCNode {
   var cpu:CCNodeColor!
   var latestTouchLocation:CGPoint?
   
+  let cpuPointsPerSecond:Double = 200
+  
   override func onEnterTransitionDidFinish() {
     super.onEnterTransitionDidFinish()
-    
-    let xImpulse = 40
-    let yImpulse = 40
-    let impulse = CGPoint(x: xImpulse, y: yImpulse)
-    ball.physicsBody.applyImpulse(impulse)
     
     self.userInteractionEnabled = true
     player.physicsBody.type = .Kinematic
     cpu.physicsBody.type = .Kinematic
+    
+    startGame()
   }
   
   override func touchBegan(touch: CCTouch!, withEvent event: CCTouchEvent!) {
@@ -31,4 +30,26 @@ class MainScene: CCNode {
     player.positionInPoints.y += yDistance
     latestTouchLocation = touchLocation
   }
+  
+  override func fixedUpdate(delta: CCTime) {
+    // cpu movement
+    if (ball.position.y < cpu.positionInPoints.y) {
+      cpu.positionInPoints = CGPoint(x: cpu.positionInPoints.x, y: cpu.positionInPoints.y - CGFloat(delta * cpuPointsPerSecond))
+    } else if (ball.position.y > cpu.positionInPoints.y) {
+      cpu.positionInPoints = CGPoint(x: cpu.positionInPoints.x, y: cpu.positionInPoints.y + CGFloat(delta * cpuPointsPerSecond))
+    }
+    
+    if ( (CGRectGetMaxX(ball.boundingBox()) < 0) || CGRectGetMinX(ball.boundingBox()) > self.contentSizeInPoints.width ){
+      startGame()
+    }
+  }
+  
+  func startGame() {
+    ball.position = CGPoint(x: self.contentSizeInPoints.width / 2, y: self.contentSizeInPoints.height / 2)
+    let xImpulse = 40
+    let yImpulse = 40
+    let impulse = CGPoint(x: xImpulse, y: yImpulse)
+    ball.physicsBody.applyImpulse(impulse)
+  }
+  
 }
